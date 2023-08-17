d76e32f73aa406e75e271e651ddadbfead39f5ba
luisbelisario <luisbls77@hotmail.com>
2023-08-17 19:40:42 -0300

    minor adjustments

--- src/main/java/com/reservei/securityapi/securityapi/config/security/SecurityFilter.java
+++ src/main/java/com/reservei/securityapi/securityapi/config/security/SecurityFilter.java
@@ -9,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
