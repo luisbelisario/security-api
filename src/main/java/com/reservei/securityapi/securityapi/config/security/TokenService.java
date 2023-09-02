2496865dcee76dc3a657a994fad26f4229c7801a
luisbelisario <luisbls77@hotmail.com>
2023-09-02 16:24:45 -0300

    refresh token adjustments

--- src/main/java/com/reservei/securityapi/securityapi/config/security/TokenService.java
+++ src/main/java/com/reservei/securityapi/securityapi/config/security/TokenService.java
@@ -57,7 +57,7 @@ public class TokenService {
     }
 
     private Instant generateExpirationDate() {
-        return LocalDateTime.now().plusMinutes(1).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 
     public String refreshToken(String token, String login) throws GenericException {
